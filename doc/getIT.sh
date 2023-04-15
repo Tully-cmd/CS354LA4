3ae4fd9c890d2746e16d7610670ab3ba8f2be513
Tully-cmd <tullymartin@u.boisestate.edu>
2023-04-15 14:59:23 -0600

    init commit

---
+++ doc/getIT.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scp -r tullymartin@onyx.boisestate.edu:~/../JBuffenb/classes/354/pub/etc/student .
+
+scp -r tullymartin@onyx.boisestate.edu:~/../JBuffenb/classes/354/pub/la4 .
+
+scp -r tullymartin@onyx.boisestate.edu:~/../JBuffenb/classes/354/pub/etc/sleepsort .
+
+scp -r tullymartin@onyx.boisestate.edu:~/../JBuffenb/classes/354/pub/sum/go .
+
+scp -r tullymartin@onyx.boisestate.edu:~/../JBuffenb/classes/354/pub/slides/slides-go.pdf .

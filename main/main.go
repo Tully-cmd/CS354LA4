7991e0b16923c534260a202abba384c483314ca9
Tully-cmd <tullymartin@u.boisestate.edu>
2023-04-15 22:31:16 -0600

    Added main

--- main/main.go
+++ main/main.go
@@ -4,5 +4,10 @@ import . "fmt"
 import . "local/bank"
 
 func main() {
+	var a ICustomer=NewCustomer("Tully")
+	var aA ICheckingAccount=NewAccount("Tully",a,4000)
+
+	Printf("a.ToString() => %s\n",a.ToSTring())
+	Printf("aA.ToString() => %s\n",aA.ToSTring())
 	
 }
